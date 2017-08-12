3d8945c5dbc4ec8409c69460ed8b029ebc1663b0
Juan Diego Torres M <jdtorretron@gmail.com>
2017-08-12 15:23:17 -0500

    fin front

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,17 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/extrapolar', function(req, res, next) {
-  res.send(req.body);
-
+  console.log(req.body);
+  var img = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+  res.render('displayResultados', { 
+    title: 'Resultados', 
+    invInicial: 30000, 
+    meses: 125, 
+    porcentaje: 20, 
+    meta: 200000000, 
+    img: img,
+    posible: true
+  });
 });
 
 module.exports = router;
